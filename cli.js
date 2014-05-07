d0f9e32ea443d0e9cd5a2250c267653827ad468a
Julien Fontanet <julien.fontanet@isonoe.net>
2014-05-07 12:33:08 +0200

    Various updates.

--- cli.js
+++ cli.js
@@ -22,7 +22,7 @@ yargs.fail(function (msg) {
 
 var parseRange = require('range-parser2');
 
-// var Promise = require('bluebird');
+var Promise = require('bluebird');
 
 var eventToPromise = require('event-to-promise');
 
@@ -32,7 +32,32 @@ require('tilelive-http').registerProtocols(tilelive);
 
 //--------------------------------------------------------------------
 
-// var dlTiles = require('./');
+var searchLocation = require('./osm').search;
+
+//====================================================================
+
+var dlTiles = function (store, bbox, zooms) {
+  var scheme = tilelive.Scheme.create('scanline', {
+    minzoom: zooms[0],
+    maxzoom: zooms[zooms.length - 1],
+    bbox: bbox && [bbox.west, bbox.south, bbox.east, bbox.north],
+  });
+
+  var task = new tilelive.CopyTask(
+    'http://tile.openstreetmap.org/{z}/{x}/{y}.png',
+    store,
+    scheme
+  );
+  task.on('progress', function (info) {
+    console.log([
+      info.processed, '/', info.total,
+      ' downloaded (', info.remaining, ' remaining)'
+    ].join(''));
+  });
+  task.start();
+
+  return eventToPromise(task, 'finished');
+};
 
 //====================================================================
 
@@ -42,9 +67,6 @@ module.exports = function (args) {
     .example(
       '$0 paris.mbtiles "Paris, France"',
       'Download tiles for a location'
-    )
-    .example(
-      '$0 custom.mbtiles'
     )
     .options({
       h: {
@@ -59,7 +81,7 @@ module.exports = function (args) {
       },
       g: {
         alias: 'global-zoom',
-        default: '0-4',
+        default: '0-3',
         describe: 'zoom level for the whole map (can be a range)',
       },
       z: {
@@ -82,43 +104,32 @@ module.exports = function (args) {
     return require('./package').version;
   }
 
+  var globalZooms = parseRange.withoutUnions(opts['global-zoom']);
   var zooms = parseRange.withoutUnions(opts.zoom);
+  var store = 'mbtiles://'+ resolvePath(opts._[0]);
 
-  return require('./osm').search(opts._[1]).then(function (info) {
-    var bbox = info.boundingbox;
-    bbox = {
-      south: +bbox[0],
-      north: +bbox[1],
-      west: +bbox[2],
-      east: +bbox[3],
-    };
+  return Promise.all([
+    dlTiles(store, null, globalZooms),
+    searchLocation(opts._[1]).then(function (info) {
+      var bbox = info.boundingbox;
+      bbox = {
+        south: +bbox[0],
+        north: +bbox[1],
+        west: +bbox[2],
+        east: +bbox[3],
+      };
 
-    console.log([
-      info.display_name,
-      '- n: '+ bbox.north,
-      '- e: '+ bbox.east,
-      '- s: '+ bbox.south,
-      '- w: '+ bbox.west,
-    ].join('\n'));
+      console.log([
+        info.display_name,
+        '- n: '+ bbox.north,
+        '- e: '+ bbox.east,
+        '- s: '+ bbox.south,
+        '- w: '+ bbox.west,
+      ].join('\n'));
 
-    return bbox;
-  }).then(function (bbox) {
-    var scheme = tilelive.Scheme.create('scanline', {
-      minzoom: zooms[0],
-      maxzoom: zooms[zooms.length - 1],
-      bbox: [bbox.west, bbox.south, bbox.east, bbox.north],
-    });
-
-    var task = new tilelive.CopyTask(
-      'http://tile.openstreetmap.org/{z}/{x}/{y}.png',
-      'mbtiles://'+ resolvePath(opts._[0]),
-      scheme
-    );
-    task.on('progress', function (info) {
-      console.log()
-    });
-    task.start();
-
-    return eventToPromise(task, 'plop');
-  });
+      return bbox;
+    }).then(function (bbox) {
+      return dlTiles(store, bbox, zooms);
+    }),
+  ]).return();
 };
